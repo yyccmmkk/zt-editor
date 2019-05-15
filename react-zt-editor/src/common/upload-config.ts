0ffec89f01dac6e5e6238e0e14712d03e3db4a9a
zhou.longfei <36995800@163.com>
2019-05-15 18:03:24 +0800

    dev

--- react-zt-editor/src/common/upload-config.ts
+++
@@ -1,9 +0,0 @@
-const UPLOAD_CONFIG={
-    action:'http://localhost:3030/',
-    name:'files',
-    access:'image/*',
-    showUploadList:false,
-
-};
-export default UPLOAD_CONFIG;
-export {UPLOAD_CONFIG}
--- react-zt-editor/src/redux/store/index.ts
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import main from '../reducers'
-let store =createStore(main);
-export {store};
-export default store;
